732555294a712288d1d52f83eb22d922fbb3790b
EulersLittleHelper <59776795+amandawang9997@users.noreply.github.com>
2020-01-11 22:53:48 -0600

    Add files via upload

--- backtracking.py
+++ backtracking.py
@@ -1,5 +1,17 @@
 import rule
-from read import printPuzzle
+from read import printPuzzle,read
+
+
+
+#the main loop so that it will always ask an input then solve the puzzle
+def mainLoop():
+        state=read()['puzzle']
+        assignment=backtracking(state)
+        temp=assignAll(assignment,state) #the final solution as a 2d list
+        print('the answer is:')
+        printPuzzle(temp)
+
+
 def backtracking(state): #state is the initial state immediately after reading from stdin
     U=rule.getUnassigned(state) #a list of all unassigned variables
     A = {} #a dictionary that maps: unassigned coordinate-->None
@@ -13,23 +25,22 @@ def backtracking(state): #state is the initial state immediately after reading f
 def backtrackingHelper(A,U,state):
     current = assignAll(A, state)
     printPuzzle(current)
-    if len(U)==0 and len(A)==len(state)*len(state[0])-len(rule.getNumbers(state)) and rule.solutionCheck(current):#all unassigned cell have been assigned
+    if rule.solutionCheck(current):#all unassigned cell have been assigned
         return A                                                                             #and the number constraint is satisfied
     elif len(U)>0:
         (i,j)=U.pop(0)
-        for option in ['_','b']:    #one coordinate has 2 possible assignment
-
-            # if option=='b' and ((i,j) not in rule.goodCells(current)):
-            #     break
-            B=A.copy()
-            B[(i,j)]=option         #add one more assignment to list A
-            # temp=assignAll(B,state) #apply assignments to original state
-
-            # if not rule.numberConstrain(temp):
-            #     break
+        for option in ['b','_']:    #one coordinate has 2 possible assignment
+            B = A.copy()
+            B[(i, j)] = option
+            if ((i,j) not in rule.goodCells(current)) and option=='b':
+                continue
+            temp=assignAll(B,state)
+            if not rule.numberConstrain(temp):
+                continue
             result=backtrackingHelper(B,U.copy(),state) #recursive call with new assignments B
             if result is not None:                  # WE HAVE no solution in this case
                 return result
+
     return None
 
 
