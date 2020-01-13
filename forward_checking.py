88e9a6ec5fe1e89b48f5491d28702c8092886284
EulersLittleHelper <59776795+amandawang9997@users.noreply.github.com>
2020-01-12 19:20:32 -0600

    Create forward_checking.py

---
+++ forward_checking.py
@@ -0,0 +1,17 @@
+#this is the implementation of forwardChecking with backtracking
+import rule
+from read import printPuzzle
+
+#this method receives a 2d-list state and outputs an assignment for all the cells
+def forwardChecking(state):
+    A,D=[],[]
+    U=rule.getUnassigned(state)
+    forwardCheckingHelper(A,U,D)
+    
+
+#
+def forwardCheckingHelper(A,U,D):
+    current = assignAll(A, state)
+    printPuzzle(current)
+    if rule.solutionCheck(current):#all unassigned cell have been assigned
+        return A
