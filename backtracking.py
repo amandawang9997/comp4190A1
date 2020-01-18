aed3628bfc9dbbc451b84231c527c9c55d8a160d
EulersLittleHelper <59776795+amandawang9997@users.noreply.github.com>
2020-01-18 15:20:58 -0600

    awd

--- backtracking.py
+++ backtracking.py
@@ -2,7 +2,7 @@
 import rule
 from read import printPuzzle, read
 import random
-
+import copy
 # the main loop so that it will always ask an input then solve the puzzle
 
 
@@ -34,18 +34,18 @@ def countHelper(A, U, state):
     def backtrackingHelper(A, U, state):
         nonlocal count
         count += 1
-        current = assignAll(A, state)
+        current = assignDict(A, state)
         printPuzzle(current)
         if rule.solutionCheck(current):  # all unassigned cell have been assigned
             return A  # and the number constraint is satisfied
         elif len(U) > 0:
-            (i, j) = U.pop(random.randint(0, len(U) - 1))
+            (i, j) = U.pop(0)
             for option in ['b', '_']:  # one coordinate has 2 possible assignment
                 B = A.copy()
                 B[(i, j)] = option
                 if ((i, j) not in rule.goodCells(current)) and option == 'b':
                     continue
-                temp = assignAll(B, state)
+                temp = assignDict(B, state)
                 if not rule.numberConstrain(temp):
                     continue
                 # recursive call with new assignments B
@@ -57,7 +57,15 @@ def countHelper(A, U, state):
     return backtrackingHelper(A, U, state), count
 
 
+def assignDict(dict, state):
+    result = copy.deepcopy(state)
+    for (i, j), option in dict.items():
+        result[i][j] = option
+    return result
+
 # this method receives an initial state and apply assignment A to it then return its copy
+
+
 def assignAll(A, state):
     result = state.copy()
     for (i, j), option in A.items():
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-#run this file to test
-from read import read,printPuzzle
-import rule as rl,backtracking as bt
+# run this file to test
+from read import read, printPuzzle
+import rule as rl
+import backtracking as bt
 
 
-#the following code solves puzzle one at a time
+# the following code solves puzzle one at a time
 bt.mainLoop()
